6199d798b91dcd8a8a82481def3cab78d34eaaa4
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-02-27 21:45:14 -0400

    finalized service worker sw.js file

---
+++ sw.js
@@ -0,0 +1,44 @@
+const cacheName = 'Tempcache2';
+// install service worker
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/script.js',
+                    '/db.js',
+                    '/manifest.json',
+                    '/styles.css',
+                    '/taskicon_400x400.png',
+                    '/dexie.js',
+                    '/favicon.ico',
+                    '/dexie.js.map',
+                ])
+            })
+    );
+    try {
+        return self.clients.claim();
+    } catch (error) {
+        console.log(error)
+        return 0
+    }
+    
+});
+
+// activat service worker
+self.addEventListener('activate', function (event) {
+    console.log('Service worker activated', event);
+})
+
+// return cached response
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(cache => caches.match(event.request, {ignoreSearch: true}))
+            .then(function (res) {
+                return res || fetch(event.request);
+            })
+    );
+});
